8a0e2de4b6e63ccfe059ee6d6cad62f8aff58d1b
Samuellekpor <samuellekpor@gmail.com>
2022-11-29 23:18:53 +0000

    Add styling to the dragon component

--- src/components/Dragon.js
+++ src/components/Dragon.js
@@ -3,12 +3,12 @@ import React from 'react';
 function Dragon(props) {
   const dragon = props;
   return (
-    <div className='dragon'>
-      <img src={dragon.image} alt="Dragon" />
+    <div className="dragon">
+      <img src={dragon.image} alt="Dragon" className="dragon-image" />
       <div className="dragon-details">
         <h3 className="dragon-name">{dragon.name}</h3>
         <p className="dragon-description">{dragon.description}</p>
-        <button className='dragon-button' type="button">Reserve Dragon</button>
+        <button className="dragon-button" type="button">Reserve Dragon</button>
       </div>
     </div>
   );
