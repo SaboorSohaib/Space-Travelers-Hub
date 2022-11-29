71fdad97fb9160f6abc7713c315d34781c50edf1
Bludidi <bludidi5@gmail.com>
2022-11-29 06:26:29 +0200

    create empty my profile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/header';
 import './App.css';
+import MyProfile from './components/myProfile';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MyProfile />
     </div>
   );
 }
