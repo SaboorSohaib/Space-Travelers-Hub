756e73c7cc96405b7e2ad846ec88dee8a264f495
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-12-01 15:53:37 +0430

    fix linter errors

--- src/components/rockets.js
+++ src/components/rockets.js
@@ -16,21 +16,21 @@ const Rockets = (item) => {
       </div>
       <div className="rocket-info">
         <h3>{rocket_name}</h3>
-          <p>
-            <span
-              className="reserve-span"
-              style={{
-                backgroundColor: reserved ? ' #4e4eff' : 'gray',
-                color: reserved ? '#fff' : '#000',
-                padding: reserved ? '3px' : '0',
-                marginRight: reserved ? '11px' : '0',
-                borderRadius: reserved ? '5px' : '0',
-              }}
-            >
-              {reserved && 'Reserved'}
-            </span>
-            {description}
-          </p>
+        <p>
+          <span
+            className="reserve-span"
+            style={{
+              backgroundColor: reserved ? ' #4e4eff' : 'gray',
+              color: reserved ? '#fff' : '#000',
+              padding: reserved ? '3px' : '0',
+              marginRight: reserved ? '11px' : '0',
+              borderRadius: reserved ? '5px' : '0',
+            }}
+          >
+            {reserved && 'Reserved'}
+          </span>
+          {description}
+        </p>
         <button
           style={{
             backgroundColor: reserved ? 'gray' : '#4e4eff',
