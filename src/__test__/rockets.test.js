2e8c769f16283364ddcf04467f0b6526e145ad28
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-12-02 12:08:19 +0430

    Add test file

---
+++ src/__test__/rockets.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import store from '../redux/configureStore';
+import Rockets from '../components/rockets';
+
+it('It should renders the  Component correctly', () => {
+  const tree = renderer.create(
+    <Provider store={store}>
+      <Rockets />
+    </Provider>,
+  );
+  expect(tree).toMatchSnapshot();
+});
