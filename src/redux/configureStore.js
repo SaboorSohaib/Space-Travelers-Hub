db23807b6565e43f67bef47fa7a0d4d001fe866f
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-12-02 19:46:12 +0430

    Resolve some conflicts

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,11 +3,13 @@ import { applyMiddleware, configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import dragonsReducer from './dragons/dragons';
 import rocketReducer from './rockets/rockets';
+import missionReducer from './missions/mission.redux';
 
 const store = configureStore({
   reducer: {
     rocket: rocketReducer,
     dragons: dragonsReducer,
+    mission: missionReducer,
   },
 }, applyMiddleware(thunk));
 
--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import missionReducer from './missions/mission.redux';
-
-export default configureStore({
-  reducer: {
-    mission: missionReducer,
-  },
-});
