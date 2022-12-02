86d34de12b0dd31897e351b81e0df2d564e4e68b
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-12-02 15:16:31 +0430

    Add reserved rocket to profile

---
+++ src/components/profileSection.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+function ProfileSection({ name }) {
+  return (
+    <div>
+      {name}
+    </div>
+  );
+}
+
+export default ProfileSection;
